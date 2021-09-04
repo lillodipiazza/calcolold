3875873d83deed865fe3cfdcba43f1f7f7df11b9
Lillo <lillodipiazza@gmail.com>
2021-09-04 19:26:38 +0200

    start

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,192 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="success" v-show="success">
+      <div class="testo">Dati Salvati</div>
+    </div>
+    <div v-if="date" class="salvataggio">
+      Salvato: {{date | moment("dddd, Do MMMM YYYY") }}
+    </div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <label><code>IERI</code></label>
+        </b-col>
+        <b-col>
+          <label><code>OGGI</code></label>
+        </b-col>
+        <b-col align-self="center">
+          <label><code>VENDUTO</code></label>
+        </b-col>
+      </b-row>      
+      <b-row>
+        <b-col>
+          <label><code>50</code></label>
+          <b-form-input type="number" v-model="dati.nopesoieri" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col>
+          <label><code>50</code></label>
+          <b-form-input type="number" v-model="dati.nopesooggi" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col align-self="center">
+          <label><code>50</code></label>
+          <b-form-input :placeholder="nopeso"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <label><code>52</code></label>
+          <b-form-input type="number" v-model="dati.leggeroieri" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col>
+          <label><code>52</code></label>
+          <b-form-input type="number" v-model="dati.leggerooggi" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col align-self="center">
+          <label><code>52</code></label>
+          <b-form-input :placeholder="leggero"></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <label><code>56</code></label>
+          <b-form-input type="number" v-model="dati.medioieri" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col>
+          <label><code>56</code></label>
+          <b-form-input type="number" v-model="dati.mediooggi" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col align-self="center">
+          <label><code>56</code></label>
+          <b-form-input :placeholder="medio"></b-form-input>
+        </b-col>
+      </b-row>   
+      <b-row>
+        <b-col>
+        </b-col>
+        <b-col>
+        </b-col>
+        <b-col align-self="center">
+          <label><code>Totale</code></label>
+          <b-form-input :placeholder="totale"></b-form-input>
+        </b-col>
+      </b-row>          
+      <b-row>
+        <b-col class="pulsanti mt-4">
+          <b-button style="margin-right: 1rem" class="w-100" variant="warning" @click="inverti">Inverti</b-button>
+          <b-button class="w-100" variant="success" @click="persist">Salva</b-button>
+        </b-col>
+      </b-row>      
+    </b-container>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      dati: {
+        leggerooggi: 0,
+        leggeroieri: 0,
+        mediooggi: 0,
+        medioieri: 0,
+        nopesooggi: 0,
+        nopesoieri: 0
+      },
+      success: false
+    }
+  },
+  mounted() {
+    if(
+      localStorage.medioieri && 
+      localStorage.mediooggi && 
+      localStorage.nopesoieri && 
+      localStorage.nopesooggi && 
+      localStorage.leggeroieri && 
+      localStorage.leggerooggi &&
+      localStorage.date
+      ) {
+      this.dati.medioieri = localStorage.medioieri;
+      this.dati.mediooggi = localStorage.mediooggi;
+      this.dati.nopesoieri = localStorage.nopesoieri;
+      this.dati.nopesooggi = localStorage.nopesooggi;
+      this.dati.leggeroieri = localStorage.leggeroieri;
+      this.dati.leggerooggi = localStorage.leggerooggi;   
+      this.date = localStorage.date   
+    }
+  },
+  methods: {
+    persist() {
+      localStorage.medioieri = this.dati.medioieri;
+      localStorage.mediooggi = this.dati.mediooggi;
+      localStorage.nopesoieri = this.dati.nopesoieri;
+      localStorage.nopesooggi = this.dati.nopesooggi;
+      localStorage.leggeroieri = this.dati.leggeroieri;
+      localStorage.leggerooggi = this.dati.leggerooggi;
+      this.success = true
+      localStorage.date = new Date()
+      setTimeout(() => {this.success = false}, 3000)    
+    },
+    inverti(){
+      this.dati.nopesoieri = localStorage.nopesooggi
+      this.dati.nopesooggi = 0
+      this.dati.leggeroieri = localStorage.leggerooggi
+      this.dati.leggerooggi = 0
+      this.dati.medioieri = localStorage.mediooggi
+      this.dati.mediooggi = 0
+    }    
+  },  
+  computed: {
+    nopeso() {
+      if (this.dati.nopesooggi > 0 ) {
+        return this.dati.nopesooggi-this.dati.nopesoieri
+      } else {
+        return 0
+      }      
+    },
+    leggero() {
+      if (this.dati.leggerooggi > 0 ) {
+      return this.dati.leggerooggi-this.dati.leggeroieri
+      } else {
+        return 0
+      }
+    },
+    medio() {
+      if (this.dati.mediooggi > 0 ) {
+      return this.dati.mediooggi-this.dati.medioieri
+      } else {
+        return 0
+      }
+    },
+    totale() {
+      return this.nopeso+this.leggero+this.medio
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.success {
+  background-color: #42b983;
+  color: #fff;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.salvataggio {
+  background-color: #42b983;
+  color: #fff;
+  padding: 10px;
+  margin-bottom: 1rem;
+}
+.pulsanti {
+  display: flex;
+}
 h3 {
   margin: 40px 0 0;
 }
